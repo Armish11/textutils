9b3f9b5251bc13059fa1b59a102629752c790c7d
Armish11 <armishsohaib216@gmail.com>
2024-02-18 19:42:20 +0500

    I have removed the search button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
+
+// import {
+//   BrowserRouter,
+//   Link,
+//   Route,
+//   Routes,
+// } from "react-router-dom";
+
 import './App.css';
+import Alert from './Components/Alert';
+import Navbar from './Components/Navbar';
+import Textform from './Components/Textform';
+// import About from './Components/About';
+import React, { useState } from 'react';
+
+// import About  from './Components/About';
+
+
 
 function App() {
+  const[alert,setAlert]=useState(null);
+const showAlert=(message,type)=>{
+  setAlert({
+    msg:message,
+    type:type,
+  }
+  )
+setTimeout(() => {
+  setAlert(null);
+},2000);
+  
+}
+  const[text, changetxt]=useState('Enable dark mode')
+const[mode, setmode]= useState('light');//Whether dark mode is enabled or not?
+const toggleMode=()=>{
+  if(mode==='light'){
+    changetxt('Enable light mode')
+    setmode('dark');
+    document.body.style.backgroundColor='#052c65';
+    showAlert("dark mode has been enabled","success");
+document.title="TextUtils-Dark Mode"    
+  }
+  else{
+    changetxt('Enable dark mode');
+    setmode('light');
+    document.body.style.backgroundColor='white';
+showAlert("light mode has been enabled","success");
+document.title='TextUtils-Light Mode'
+
+  }
+}
+//   const[gtext, gchangetxt]=useState('Enable green mode')
+// const[gmode, gsetmode]= useState('red');//Whether green mode is enabled or not?
+// const gtoggleMode=()=>{
+//   if(gmode==='red'){
+//     gchangetxt('Enable red mode')
+//     gsetmode('green');
+//     document.body.style.backgroundColor='#65a565';
+//     showAlert("Green mode has been enabled","success");
+
+    
+//   }
+//   else{
+//     gchangetxt('Enable green mode');
+//     gsetmode('red');
+//     document.body.style.backgroundColor='red';
+// showAlert("Red mode has been enabled","success");
+//   }
+ 
+// }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    {/* // <BrowserRouter> */}
+<Navbar title="TextUtils" abouttext="About" mode={mode} toggleMode={toggleMode} text={text} /*gmode={gmode} gtoggleMode={gtoggleMode} gtext={gtext}*//>
+{/* <Navbar/> */}
+<Alert alert={alert}/>
+<div className="container my-3">
+{/* <Routes> */}
+  {/* React always do partial matching so you have to exact path to reach the desired path */}
+          {/* <Route exact path="/about" element={<About />}/> */}
+
+          <Textform mode={mode}  toggleMode={toggleMode} /*gmode={gmode} gtoggleMode={gtoggleMode}*/ heading={`Enter the text to analyze here`} showAlert={showAlert}/>
+
+      {/* </Routes> */}
+</div>
+{/* // </BrowserRouter> */}
+</>
+
+
+  
+      );
 }
 
 export default App;
---
+++ src/Components/Textform.js
@@ -0,0 +1,76 @@
+import React, {useState} from 'react'
+
+export default function Textform(props) {
+    const[text,settext]=useState('');
+
+    const handleonchange=(event)=>{
+console.log("text on change");
+settext(event.target.value);
+
+    }
+    const handleupclick=()=>{
+        console.log("Uppercase was clicked"+ text);
+        let newtext=text.toUpperCase()
+        settext(newtext);
+        props.showAlert("Converted to Uppercase","success")
+    }
+    const handledownclick=()=>{
+        console.log("Lowercase was clicked"+ text);
+        let newtext=text.toLowerCase();
+        settext(newtext);
+        props.showAlert("Converted to lowercase","success")
+    }
+    const handlereplace=()=>{
+        let replace=prompt('What word you want to replace?');
+        let replacewith=prompt('Enter the word you want in that place.');
+        let newtext=text.replaceAll(replace,replacewith);
+        settext(newtext);
+        props.showAlert("Text has been replaced","success")
+    }
+    const handlecleartext=()=>{
+        let newtext=""
+        settext(newtext);
+        props.showAlert("Text has been cleared","success")
+    }
+    const handlecopytext=(event)=>{
+        let text1=document.getElementById("myBox");
+        text1.select();
+        text1.setSelectionRange(0,Infinity);
+        navigator.clipboard.writeText(text1.value);
+        props.showAlert("Text has been copied to clipboard","success")
+    }
+const removeextraspace=()=>{
+let text2=/[ ]+/g;
+let newtext=text.replace(text2," ");
+settext(newtext);
+props.showAlert("Extra whitespaces has been removed","success")
+}
+    
+  return (
+    <>
+    <div>
+<div className="mb-3" style={{color:props.mode==='dark'?'white':'black'}}>
+  <label htmlFor="myBox" className="form-label" ><h1>{props.heading}</h1></label>
+  <textarea className="form-control" id="myBox" value={text} onChange={handleonchange} rows="8"
+style={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'black'}}></textarea>
+</div>
+<button className="btn btn-primary mx-1 my-1" onClick={handleupclick} >Convert to Uppercase</button>
+<button className="btn btn-primary mx-1 my-1"  onClick={handledownclick}>Convert to Lowercase</button>
+<button className=" btn btn-primary mx=1 my-1" onClick={handlereplace}>Replace Words</button>
+<button className="btn btn-primary mx-1 my-1" onClick={handlecleartext}>Clear Text</button>
+<button className="btn btn-primary mx-1 my-1" onClick={handlecopytext}>Copy Text</button>
+<button className="btn btn-primary mx-1 my-1" onClick={removeextraspace}>Remove Extra Spaces</button>
+    </div>
+    <div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}}>
+    <h1>Your text summary</h1>
+        <p className='my-2' >{text===""?0:text.trim().split(/[ ]+/g).length} words and {text.trim().replace(/[ ]+ /g, '').length} characters.</p>
+        <p>{0.008 * (text===""?0:text.trim().split(/[ ]+/g).length) } Minutes to read</p>
+        <p>{text===""?0:text.trim().split('. ').length} number of sentences</p>
+    </div>
+    <div className="container" style={{color:props.mode==='dark'?'white':'black'}}>
+        <h2>Preview</h2>
+        <p>{text.length>0?text:'Enter something in the textbox above to preview it here'}</p>
+    </div>
+    </>
+  )
+}
